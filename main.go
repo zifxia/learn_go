a415b6641c5bb0fc5c70153c8e79bf4ad7b11983
zyio0 <xia.zi@husky.neu.edu>
2020-01-29 02:04:42 -0800

    test lazygit function in bashrc

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	// "os"
+)
+
+func main() {
+	// fmt.Println(os.Args)
+	// if len(os.Args) != 2 {
+	// 	os.Exit(1)
+	// }
+	// fmt.Println("It's over", os.Args[1])
+
+	var power int
+	power = 9000
+	fmt.Printf("It's over %v\n", power)
+}
+	
